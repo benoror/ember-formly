d294677a182520431d9a4eb610e8cad2841185de
Ben Orozco <benoror@gmail.com>
2016-10-09 23:49:36 -0500

    Feat[dummy] Add JSON document Model

---
+++ tests/dummy/app/models/form.js
@@ -0,0 +1,9 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+// import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  document: attr({
+    defaultValue: () => { return {}; }
+  })
+});
--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -2,97 +2,97 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return {
-      fields: [
-        {
-          key: 'email',
-          component: 'ember-formly-fields/html-input',
-          options: {
-            type: 'email',
-            label: 'Email address',
-            placeholder: 'Enter email'
-          }
-        },
-        {
-          key: 'password',
-          component: 'ember-formly-fields/html-input',
-          options: {
-            type: 'password',
-            label: 'Password',
-            placeholder: 'Password'
-          }
-        },
-        {
-          key: 'checked',
-          component: 'ember-formly-fields/html-input',
-          options: {
-            type: 'checkbox',
-            label: 'Check me out'
-          }
-        },
-        {
-          key: 'slider',
-          component: 'ember-formly-fields/html-input',
-          options: {
-            type: 'range',
-            label: 'Slide me out',
-            min: 1,
-            max: 5
-          }
-        },
-        {
-          key: 'chooseone',
-          component: 'radios-inputs',
-          options: {
-            label: 'Chose one (radios)',
-            inputs: [
-              {key: 'a', label: 'Option A'},
-              {key: 'b', label: 'Option B'},
-              {key: 'c', label: 'Option C'}
-            ]
-          }
-        },
-        {
-          key: 'choosemany',
-          component: 'checkboxes-inputs',
-          options: {
-            label: 'Chose many (checkboxes)',
-            inputs: [
-              {key: 'w', label: 'Option W'},
-              {key: 'x', label: 'Option X'},
-              {key: 'y', label: 'Option Y'},
-              {key: 'z', label: 'Option Z'}
-            ]
-          }
-        },
-        {
-          key: 'optionaltext',
-          component: 'three-state-textbox',
-          options: {
-            label: 'Optional text (three-state-textbox)',
-            inputs: [
-              {key: 'w', label: 'Option W'},
-              {key: 'x', label: 'Option X'},
-              {key: 'y', label: 'Option Y'},
-              {key: 'z', label: 'Option Z'}
-            ]
-          }
-        }
-      ],
-      model: {
+    return this.get('store').createRecord('form', {
+      document: {
         email: 'my@email.com',
-        password: 's3cre7',
+        //password: 's3cre7',
         checked: true,
-        slider: 4,
-        chooseone: 'c',
-        choosemany: { w:false, x:true, y:false, z:true },
-        optionaltext: { active:true, text:'Ok' }
+        //slider: 4,
+        chooseone: 'c'
+        //choosemany: { w:false, x:true, y:false, z:true },
+        //optionaltext: { active:true, text:'Ok' }
       }
-    };
+    });
   },
 
   setupController(controller, model) {
     this._super(...arguments);
-    controller.setProperties(model);
+    const fields = [
+      {
+        key: 'email',
+        component: 'ember-formly-fields/html-input',
+        options: {
+          type: 'email',
+          label: 'Email address',
+          placeholder: 'Enter email'
+        }
+      },
+      {
+        key: 'password',
+        component: 'ember-formly-fields/html-input',
+        options: {
+          type: 'password',
+          label: 'Password',
+          placeholder: 'Password'
+        }
+      },
+      {
+        key: 'checked',
+        component: 'ember-formly-fields/html-input',
+        options: {
+          type: 'checkbox',
+          label: 'Check me out'
+        }
+      },
+      {
+        key: 'slider',
+        component: 'ember-formly-fields/html-input',
+        options: {
+          type: 'range',
+          label: 'Slide me out',
+          min: 1,
+          max: 5
+        }
+      },
+      {
+        key: 'chooseone',
+        component: 'radios-inputs',
+        options: {
+          label: 'Chose one (radios)',
+          inputs: [
+            {key: 'a', label: 'Option A'},
+            {key: 'b', label: 'Option B'},
+            {key: 'c', label: 'Option C'}
+          ]
+        }
+      },
+      {
+        key: 'choosemany',
+        component: 'checkboxes-inputs',
+        options: {
+          label: 'Chose many (checkboxes)',
+          inputs: [
+            {key: 'w', label: 'Option W'},
+            {key: 'x', label: 'Option X'},
+            {key: 'y', label: 'Option Y'},
+            {key: 'z', label: 'Option Z'}
+          ]
+        }
+      },
+      {
+        key: 'optionaltext',
+        component: 'three-state-textbox',
+        options: {
+          label: 'Optional text (three-state-textbox)',
+          inputs: [
+            {key: 'w', label: 'Option W'},
+            {key: 'x', label: 'Option X'},
+            {key: 'y', label: 'Option Y'},
+            {key: 'z', label: 'Option Z'}
+          ]
+        }
+      }
+    ];
+    controller.set('fields', fields);
   }
 });
